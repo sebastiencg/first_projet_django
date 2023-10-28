1a92702a0f493e60c33f1cae04250bff7105bd1a
sebastien <miantamag@gmail.com>
2023-10-28 18:49:59 +0200

    new

--- siteweb/api/urls.py
+++ siteweb/api/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('message', views.get_messages, name='api_find_all'),
     path('message/new', views.new_message),
     path('message/<str:id>', views.get_message),
+    path('message/update/<str:id>', views.update_Message),
+    path('message/delete/<str:id>', views.delete_Message),
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 
--- siteweb/api/views.py
+++ siteweb/api/views.py
@@ -34,3 +34,36 @@ def new_message(request):
             message.save()
             return Response(message.data)
     return Response('error')
+
+
+@permission_classes([IsAuthenticated])
+@api_view(['PUT', 'PATCH'])
+def update_Message(request, id):
+    if request.method == 'PUT' or request.method == 'PATCH':
+        message = Message.objects.get(id=id)
+        user = request.user.id
+
+        if message.author.id != user:
+            return Response('Vous n\'êtes pas autorisé à mettre à jour ce message.')
+
+        serializer = MessageDeSerializer(instance=message, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response('Erreur de validation des données')
+
+    return Response('Méthode non autorisée')
+
+
+@permission_classes([IsAuthenticated])
+@api_view(['DELETE'])
+def delete_Message(request,id):
+    message = Message.objects.get(id=id)
+    user = request.user.id
+
+    if message and message.author.id == user:
+        message.delete()
+        return Response('message delete')
+
+    return Response('Error')
+
