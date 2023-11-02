79c0221214b3de4acf3babfe35f81b1a03050d81
sebastien <miantamag@gmail.com>
2023-11-02 17:48:40 +0100

    new

--- firstProjet/settings.py
+++ firstProjet/settings.py
@@ -85,23 +85,17 @@ WSGI_APPLICATION = 'firstProjet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'sebastien',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-# DATABASES = {
-#   'default': {
-# 'ENGINE': 'django.db.backends.postgresql',
-#  'NAME': 'django',
-#   'USER': 'sebastien',
-#    'PASSWORD': '1234',
-#     'HOST': '127.0.0.1',
-#      'PORT': '',
-#   }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
